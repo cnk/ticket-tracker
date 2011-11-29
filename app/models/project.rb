1888f7611c6d1a11f865417899e78a2198d36957
Cynthia Kiser <cnk@ugcs.caltech.edu>
2011-11-28 23:12:22 -0800

    annotate models using annotate gem

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Project < ActiveRecord::Base
   has_many :tickets
   validates :name, :presence => true, 
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tickets
+#
+#  id          :integer         not null, primary key
+#  title       :string(255)
+#  description :text
+#  project_id  :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#  user_id     :integer
+#
+
 require 'spec_helper'
 
 describe Ticket do
